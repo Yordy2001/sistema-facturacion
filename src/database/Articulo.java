0e92a4aee43e632269645a7626d1d63ae099edff
Yordy2001 <kingyordi2001@gmail.com>
2023-02-10 16:13:25 -0400

    create articulo table, insert and delete method:

---
+++ src/database/Articulo.java
@@ -0,0 +1,128 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+public class Articulo {
+
+    Connection cursor;
+
+    public Articulo(Connection cn) {
+        cursor = cn;
+    }
+
+
+    public void articuloTable() {
+        try {
+            String query = "CREATE TABLE if not exists articulos(\n"
+                    + "    id int NOT NULL AUTO_INCREMENT,\n"
+                    + "    name varchar(255) NOT NULL,\n"
+                    + "    description varchar(255),\n"
+                    + "    code varchar(255),\n"
+                    + "    cantidad int,\n"
+                    + "    precio_compra float NOT NULL ,\n"
+                    + "    precio_venta float,\n"
+                    + "    itbis int,\n"
+                    + "    id_category int,\n"
+                    + "    PRIMARY KEY (ID),\n"
+                    + "    FOREIGN KEY(id_category) REFERENCES articulo_category(id)"
+                    + ");";
+            PreparedStatement quer = cursor.prepareStatement(query);
+            int n = quer.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(empleadoM.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+//    public void insertArticulo(String last_name, String first_name,
+//            int age, String Address, String code_empleado, String user_password, String cargo) {
+//        try {
+//            String query = "INSERT INTO articulos(last_name,"
+//                    + " first_name, age, address, code_empleado, password, cargo ) VALUES"
+//                    + "('" + last_name + "', '" + first_name + "', '" + age + "','"
+//                    + Address + "', '" + code_empleado + "', '" + user_password + "', '" + cargo + "');";
+//            
+//            PreparedStatement sqlquery = cursor.prepareStatement(query);
+//            int n = sqlquery.executeUpdate();
+//            JOptionPane.showMessageDialog(null, "Empleado agregado!");
+//        } catch (SQLException e) {
+//            System.out.print(e);
+//            JOptionPane.showMessageDialog(null, "Error de base de datos");
+//        }
+//    }
+
+
+//
+//    public boolean delete_empleado(String codeEmpleado) {
+//        try {
+//            String query = "DELETE FROM empleados where code_empleado='" + codeEmpleado + "'";
+//            PreparedStatement sqlquery;
+//            sqlquery = cursor.prepareStatement(query);
+//            int n = sqlquery.executeUpdate(query);
+//            if (n != 1) {
+//                JOptionPane.showMessageDialog(null, "CREDENCIALES INVALIDAS!");
+//            }
+//        } catch (SQLException e) {
+//            System.out.println(e);
+//            JOptionPane.showMessageDialog(null, "ERROR: COMUNIQUESE CON UN TECNICO");
+//            return false;
+//        }
+//        return true;
+//    }
+//
+//    public void update_empleado(String code, String last_name, String first_name,
+//            int age, String address, String cargo) {
+//        try {
+//            String query = "UPDATE empleados SET last_name='" + last_name + "', first_name='" + first_name + "', age='" + age + "', address='" + address + "' ,cargo='" + cargo + "' where code_empleado='" + code + "'";
+//            PreparedStatement sqlquery;
+//            sqlquery = cursor.prepareStatement(query);
+//            int n = sqlquery.executeUpdate(query);
+//            if (n != 1) {
+//                JOptionPane.showMessageDialog(null, "CREDENCIALES INVALIDAS!");
+//            }
+//            JOptionPane.showMessageDialog(null, "Empleado actualizado con exito");
+//        } catch (SQLException e) {
+//            System.out.println(e);
+//            JOptionPane.showMessageDialog(null, "ERROR: COMUNIQUESE CON UN TECNICO");
+//        }
+//    }
+//
+    public ResultSet getArticulo(int id) {
+        ResultSet res = null;
+        try {
+            String query = "SELECT name, description, code, cantidad, precio_venta,"
+                    + " precio_compra, itbis, articulos_category.category FROM articulos"
+                    + " JOIN articulo_category ON articulos.id_category = articulo_category.id"
+                    + " WHERE articulos.id= '"+ id +"'";
+            PreparedStatement sqlquery = cursor.prepareStatement(query);
+            res = sqlquery.executeQuery(query);
+
+            return res;
+        } catch (SQLException e) {
+            System.out.println(e);
+            JOptionPane.showMessageDialog(null, "ERROR: COMUNIQUESE CON UN TECNICO");
+        }
+        return res;
+    }
+//
+//    public ResultSet getEmpleado(String code) {
+//        ResultSet res = null;
+//        try {
+//            String query = "SELECT * from empleados where code_empleado='" + code + "'";
+//            PreparedStatement sqlquery = cursor.prepareStatement(query);
+//            res = sqlquery.executeQuery(query);
+//
+//            return res;
+//        } catch (SQLException e) {
+//            System.out.println(e);
+//            JOptionPane.showMessageDialog(null, "ERROR: COMUNIQUESE CON UN TECNICO");
+//        }
+//        return res;
+//    }
+
+}
--- src/database/connect.java
+++ src/database/connect.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package database;
 
 //import empleadoM;
@@ -30,6 +25,8 @@ public class connect {
     public connect() {
         Connection cn = conexion();
         empleadoM empleado = new empleadoM(cn);
+        Articulo articulo = new Articulo(cn);
+        articulo.articuloTable();
         empleado.empleado_table(cn);
     }
 
